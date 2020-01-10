820d1edd39b85025b22ee1a427919f1f6046784f
Ioana Chivu <ioana.a.chivu@accenture.com>
2020-01-10 15:03:10 +0200

    5

---
+++ cunningBear/.sfdx/typings/lwc/sobjects/Session_Speaker__c.d.ts
@@ -0,0 +1,60 @@
+declare module "@salesforce/schema/Session_Speaker__c.CreatedBy" {
+  const CreatedBy:any;
+  export default CreatedBy;
+}
+declare module "@salesforce/schema/Session_Speaker__c.CreatedById" {
+  const CreatedById:any;
+  export default CreatedById;
+}
+declare module "@salesforce/schema/Session_Speaker__c.CreatedDate" {
+  const CreatedDate:any;
+  export default CreatedDate;
+}
+declare module "@salesforce/schema/Session_Speaker__c.Id" {
+  const Id:any;
+  export default Id;
+}
+declare module "@salesforce/schema/Session_Speaker__c.IsDeleted" {
+  const IsDeleted:boolean;
+  export default IsDeleted;
+}
+declare module "@salesforce/schema/Session_Speaker__c.LastActivityDate" {
+  const LastActivityDate:any;
+  export default LastActivityDate;
+}
+declare module "@salesforce/schema/Session_Speaker__c.LastModifiedBy" {
+  const LastModifiedBy:any;
+  export default LastModifiedBy;
+}
+declare module "@salesforce/schema/Session_Speaker__c.LastModifiedById" {
+  const LastModifiedById:any;
+  export default LastModifiedById;
+}
+declare module "@salesforce/schema/Session_Speaker__c.LastModifiedDate" {
+  const LastModifiedDate:any;
+  export default LastModifiedDate;
+}
+declare module "@salesforce/schema/Session_Speaker__c.Name" {
+  const Name:string;
+  export default Name;
+}
+declare module "@salesforce/schema/Session_Speaker__c.Session__c" {
+  const Session__c:any;
+  export default Session__c;
+}
+declare module "@salesforce/schema/Session_Speaker__c.Session__r" {
+  const Session__r:any;
+  export default Session__r;
+}
+declare module "@salesforce/schema/Session_Speaker__c.Speaker__c" {
+  const Speaker__c:any;
+  export default Speaker__c;
+}
+declare module "@salesforce/schema/Session_Speaker__c.Speaker__r" {
+  const Speaker__r:any;
+  export default Speaker__r;
+}
+declare module "@salesforce/schema/Session_Speaker__c.SystemModstamp" {
+  const SystemModstamp:any;
+  export default SystemModstamp;
+}
---
+++ cunningBear/.sfdx/typings/lwc/sobjects/Session__c.d.ts
@@ -0,0 +1,72 @@
+declare module "@salesforce/schema/Session__c.CreatedBy" {
+  const CreatedBy:any;
+  export default CreatedBy;
+}
+declare module "@salesforce/schema/Session__c.CreatedById" {
+  const CreatedById:any;
+  export default CreatedById;
+}
+declare module "@salesforce/schema/Session__c.CreatedDate" {
+  const CreatedDate:any;
+  export default CreatedDate;
+}
+declare module "@salesforce/schema/Session__c.Description__c" {
+  const Description__c:string;
+  export default Description__c;
+}
+declare module "@salesforce/schema/Session__c.Id" {
+  const Id:any;
+  export default Id;
+}
+declare module "@salesforce/schema/Session__c.IsDeleted" {
+  const IsDeleted:boolean;
+  export default IsDeleted;
+}
+declare module "@salesforce/schema/Session__c.LastActivityDate" {
+  const LastActivityDate:any;
+  export default LastActivityDate;
+}
+declare module "@salesforce/schema/Session__c.LastModifiedBy" {
+  const LastModifiedBy:any;
+  export default LastModifiedBy;
+}
+declare module "@salesforce/schema/Session__c.LastModifiedById" {
+  const LastModifiedById:any;
+  export default LastModifiedById;
+}
+declare module "@salesforce/schema/Session__c.LastModifiedDate" {
+  const LastModifiedDate:any;
+  export default LastModifiedDate;
+}
+declare module "@salesforce/schema/Session__c.LastReferencedDate" {
+  const LastReferencedDate:any;
+  export default LastReferencedDate;
+}
+declare module "@salesforce/schema/Session__c.LastViewedDate" {
+  const LastViewedDate:any;
+  export default LastViewedDate;
+}
+declare module "@salesforce/schema/Session__c.Level__c" {
+  const Level__c:string;
+  export default Level__c;
+}
+declare module "@salesforce/schema/Session__c.Name" {
+  const Name:string;
+  export default Name;
+}
+declare module "@salesforce/schema/Session__c.Owner" {
+  const Owner:any;
+  export default Owner;
+}
+declare module "@salesforce/schema/Session__c.OwnerId" {
+  const OwnerId:any;
+  export default OwnerId;
+}
+declare module "@salesforce/schema/Session__c.Session_Date__c" {
+  const Session_Date__c:any;
+  export default Session_Date__c;
+}
+declare module "@salesforce/schema/Session__c.SystemModstamp" {
+  const SystemModstamp:any;
+  export default SystemModstamp;
+}
